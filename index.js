cc4af37481074ac36845895246ee923cba038534
sanober riaz <sanober@sanober.me>
2018-01-14 23:42:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }else if (string === "I love you, Grandma") {
-    return "I lvoe you, too."
+    return "I love you, too."
   }else {
     return "YES INDEED!"
   }
